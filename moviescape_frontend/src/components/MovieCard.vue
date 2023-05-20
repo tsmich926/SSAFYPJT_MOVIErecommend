b929fdc6e1323fabb3d7d6b7a14b3a87b33b5132
ImhyeonSon <soh2254@gmail.com>
2023-05-20 21:20:34 +0900

    카드 배경 수정

--- moviescape_frontend/src/components/MovieCard.vue
+++ moviescape_frontend/src/components/MovieCard.vue
@@ -1,18 +1,12 @@
 <template>
-  <div>
-    <!-- <div class="card-body">
-      <h1 class="card-title">{{CARDmovie.title}}</h1>
-      <p class="card-text">{{CARDmovie.overview}}</p>
-    </div> -->
-    <!--  -->
-    <div class="card" style="width: 18rem;">
+  <div class="container">
+    <div class="card bg-secondary" style="width: 18rem;">
       <img :src="`https://image.tmdb.org/t/p/w500/${CARDmovie.poster_path}`" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">{{CARDmovie.title}}</h5>
+        <h5 class="card-title my-card-title">{{CARDmovie.title}}</h5>
         <p class="card-text my-card-text">{{CARDmovie.overview}}</p>
       </div>
     </div>
-<!--  -->
   </div>
 </template>
 
@@ -40,5 +34,9 @@ export default {
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
+}
+.my-card-title {
+  display: inline-block;
+  word-wrap: break-word;
 }
 </style>
--- moviescape_frontend/src/components/MovieListItems.vue
+++ moviescape_frontend/src/components/MovieListItems.vue
@@ -2,23 +2,6 @@
   <div>
     <h1>MovieListItems</h1>
     <div>
-      <div class="card" style="width: 18rem;">
-        <img :src="`https://image.tmdb.org/t/p/w500/`" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">weeeeeeeeeeeeeeeeeeesdfzsdfasfdasdfasdf</h5>
-          <p class="card-text">weeeeeeeeeeeeeeeeeeesdfzsdfasfdasdfasdfweeeeeeeeeeeeeeeeeeesdfzsdfasfdasdfasdfweeeeeeeeeeeeeeeeeeesdfzsdfasfdasdfasdf</p>
-        </div>
-      </div>
-      <div class="card" style="width: 18rem;">
-        <img src="`https://image.tmdb.org/t/p/w500/`" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>      
-    </div>
-    <div>
       <div v-for="movie in ITEMmovies" :key="movie.pk" >
         <!-- <MovieCard :CARDmovie="movie"/> -->
         <MovieCard :CARDmovie="movie"/>
